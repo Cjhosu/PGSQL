0fb1b1dcd7a5d4c26f715261cc522175fb3351e4
Caleb Howard <choward@covermymeds.com>
2018-11-12 20:46:54 -0500

    Raise different conditons on success and failure

--- archivist/archive_schema_diffs.sql
+++ archivist/archive_schema_diffs.sql
@@ -79,19 +79,25 @@ DECLARE var text;
         var2 text;
         schema_dif record;
 BEGIN
-WITH missing_tbl AS(
-SELECT p.table_name FROM information_schema.tables p
-  LEFT JOIN information_schema.tables a
-    ON p.table_name = a.table_name
-   AND p.table_schema = 'public'
-   AND a.table_schema = 'archive'
- WHERE a.table_name is null  and p.table_schema = 'public'  and p.table_name in ('prescriptions','actions','messages','message_contents')limit 1)
-SELECT * from missing_tbl INTO schema_dif;
-   var := 'SELECT fn_script_archive_tables ('''||schema_dif.table_name ||''');';
-BEGIN
- EXECUTE var into var2;
-Return var2;
-END;
+  WITH missing_tbl AS(
+  SELECT p.table_name FROM information_schema.tables p
+    LEFT JOIN information_schema.tables a
+      ON p.table_name = a.table_name
+     AND p.table_schema = 'public'
+     AND a.table_schema = 'archive'
+   WHERE a.table_name is null  and p.table_schema = 'public'  and p.table_name in ('prescriptions','actions','messages','message_contents')limit 1)
+  SELECT * from missing_tbl INTO schema_dif;
+
+  var := 'SELECT fn_script_archive_tables ('''||schema_dif.table_name ||''');';
+  BEGIN
+    IF var IS NULL
+    THEN
+      var2 := NULL;
+    ELSE
+      EXECUTE var into var2;
+    END IF;
+    RETURN var2;
+  END;
 END ;
 $$
 Language plpgsql;
@@ -165,16 +171,23 @@ RETURNS void AS
 $$
 DECLARE out_var text;
 BEGIN
-WHILE 1=1
-LOOP
-BEGIN
- EXECUTE 'SELECT fn_find_archive_tables();' INTO out_var;
-EXECUTE out_var;
-EXCEPTION WHEN OTHERS THEN
+  WHILE 1=1
+  LOOP
+    BEGIN
+      EXECUTE 'SELECT fn_find_archive_tables();' INTO out_var;
+      IF out_var IS NULL
+      THEN
         RAISE NOTICE 'All tables created';
-RETURN;
-END;
-END LOOP;
+        RETURN;
+      ELSE
+        EXECUTE out_var;
+      END IF;
+      EXCEPTION WHEN OTHERS
+      THEN
+        RAISE NOTICE 'Something has gone awry while creating tables!';
+        RETURN;
+    END;
+  END LOOP;
 END;
 $$
 Language plpgsql;
@@ -184,16 +197,23 @@ RETURNS void AS
 $$
 DECLARE out_var text;
 BEGIN
-WHILE 1=1
-LOOP
-BEGIN
- EXECUTE 'SELECT fn_script_archive_columns();' INTO out_var;
-EXECUTE out_var;
-EXCEPTION WHEN OTHERS THEN
+  WHILE 1=1
+  LOOP
+    BEGIN
+      EXECUTE 'SELECT fn_script_archive_columns();' INTO out_var;
+      IF out_var IS  NULL
+      THEN
         RAISE NOTICE 'All columns added';
-RETURN;
-END;
-END LOOP;
+        RETURN;
+      ELSE
+        EXECUTE out_var;
+      END IF;
+      EXCEPTION WHEN OTHERS
+      THEN
+        RAISE NOTICE 'Something has gone awry while creating columns!';
+        RETURN;
+    END;
+  END LOOP;
 END;
 $$
 Language plpgsql;
@@ -203,16 +223,23 @@ RETURNS void AS
 $$
 DECLARE out_var text;
 BEGIN
-WHILE 1=1
-LOOP
-BEGIN
- EXECUTE 'SELECT fn_script_data_type_diffs();' INTO out_var;
-EXECUTE out_var;
-EXCEPTION WHEN OTHERS THEN
+  WHILE 1=1
+  LOOP
+    BEGIN
+      EXECUTE 'SELECT fn_script_data_type_diffs();' INTO out_var;
+      IF out_var IS NULL
+      THEN
         RAISE NOTICE 'All types match';
-RETURN;
-END;
-END LOOP;
+        RETURN;
+      ELSE
+        EXECUTE out_var;
+      END IF;
+      EXCEPTION WHEN OTHERS
+      THEN
+        RAISE NOTICE 'Something has gone awry while reconciling data types!';
+        RETURN;
+    END;
+  END LOOP;
 END;
 $$
 Language plpgsql;
@@ -221,10 +248,10 @@ CREATE OR REPLACE FUNCTION fn_reconcile_schema()
 RETURNS void AS
 $$
 BEGIN
-CREATE SCHEMA IF NOT EXISTS archive;
-EXECUTE 'SELECT fn_create_archive_tables();';
-EXECUTE 'SELECT fn_create_archive_columns();';
-EXECUTE 'SELECT fn_update_data_types();';
+  CREATE SCHEMA IF NOT EXISTS archive;
+  EXECUTE 'SELECT fn_create_archive_tables();';
+  EXECUTE 'SELECT fn_create_archive_columns();';
+  EXECUTE 'SELECT fn_update_data_types();';
 END;
 $$
 Language plpgsql;
